dc654cfb385c53196acb089d5aa441fa48985229
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-06-24 20:06:39 +0000

    feat(scripts): added http server python script

---
+++ scripts/run_http_servers.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from threading import Thread
+from os.path import abspath, dirname, join, isdir
+from functools import partial
+import signal
+from typing import AnyStr
+
+def start_server(server: HTTPServer, dir: AnyStr) -> None:
+    try:
+        print(f"Started server on {server.server_address[0]}:{server.server_port} for {dir}")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    base_dir = abspath(dirname(__file__))
+    servers: list[HTTPServer] = []
+    threads: list[Thread] = []
+
+    # doc_dir = abspath(join(base_dir, "../docs/html"))
+    # if isdir(doc_dir):
+    #     doc_server = HTTPServer(("", 8000), partial(SimpleHTTPRequestHandler,
+    #                                                 directory=doc_dir))
+    #     servers.append(doc_server)
+    #
+    #     doc_thread = Thread(target=start_server, args=(doc_server,doc_dir))
+    #     doc_thread.start()
+    #     threads.append(doc_thread)
+    # else:
+    #     print(f"{doc_dir} does not exist, so no server was started")
+
+    cov_dir = abspath(join(base_dir, "../coverage/html"))
+    if isdir(cov_dir):
+        cov_server = HTTPServer(("", 8001), partial(SimpleHTTPRequestHandler,
+                                                    directory=cov_dir))
+        servers.append(cov_server)
+
+        cov_thread = Thread(target=start_server, args=(cov_server,cov_dir))
+        cov_thread.start()
+        threads.append(cov_thread)
+    else:
+        print(f"{cov_dir} does not exist, so no server was started")
+
+
+    if len(servers) == 0 and len(threads) ==0:
+        print("No servers created, exiting...")
+        exit(0)
+
+    try:
+        signal.pause()
+    except KeyboardInterrupt:
+        print("Shutting down servers...")
+        for server in servers:
+            server.shutdown()
+    finally:
+        for thread in threads:
+            thread.join()
+        exit(0)
