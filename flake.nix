7b625e6a9c7a5b3ecbf98f451a6096b2302c3635
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-06-24 20:06:03 +0000

    feat(flake): added nix integration

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+
+      overlays = [(import rust-overlay)];
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "o2c";
+      packages = with pkgs; [
+        cargo-binutils
+        (rust-bin.stable.latest.default.override {
+          extensions = ["rust-src" "rust-analyzer" "llvm-tools-preview"];
+        })
+      ];
+    };
+  };
+}
