258ff22979af30ed626ae4f470130e3e9a8f3286
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-06-29 19:41:48 +0000

    feat(scripts): changed how the docs server is managed

--- scripts/run_http_servers.py
+++ scripts/run_http_servers.py
@@ -7,6 +7,15 @@ from functools import partial
 import signal
 from typing import AnyStr
 
+class DocHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, *args, directory=None, **kwargs):
+        super().__init__(*args, directory=directory, **kwargs)
+
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/o2c"
+        return super().do_GET()
+
 def start_server(server: HTTPServer, dir: AnyStr) -> None:
     try:
         print(f"Started server on {server.server_address[0]}:{server.server_port} for {dir}")
@@ -14,25 +23,24 @@ def start_server(server: HTTPServer, dir: AnyStr) -> None:
     except KeyboardInterrupt:
         pass
 
-
 if __name__ == "__main__":
     base_dir = abspath(dirname(__file__))
     servers: list[HTTPServer] = []
     threads: list[Thread] = []
 
-    # doc_dir = abspath(join(base_dir, "../docs/html"))
-    # if isdir(doc_dir):
-    #     doc_server = HTTPServer(("", 8000), partial(SimpleHTTPRequestHandler,
-    #                                                 directory=doc_dir))
-    #     servers.append(doc_server)
-    #
-    #     doc_thread = Thread(target=start_server, args=(doc_server,doc_dir))
-    #     doc_thread.start()
-    #     threads.append(doc_thread)
-    # else:
-    #     print(f"{doc_dir} does not exist, so no server was started")
+    doc_dir = abspath(join(base_dir, "../target/doc"))
+    if isdir(doc_dir):
+        doc_server = HTTPServer(("", 8000), partial(DocHTTPRequestHandler,
+                                                    directory=doc_dir))
+        servers.append(doc_server)
 
-    cov_dir = abspath(join(base_dir, "../coverage/html"))
+        doc_thread = Thread(target=start_server, args=(doc_server,doc_dir))
+        doc_thread.start()
+        threads.append(doc_thread)
+    else:
+        print(f"{doc_dir} does not exist, so no server was started")
+
+    cov_dir = abspath(join(base_dir, "../target/debug/coverage"))
     if isdir(cov_dir):
         cov_server = HTTPServer(("", 8001), partial(SimpleHTTPRequestHandler,
                                                     directory=cov_dir))
