420e7f63edc541375efa8061f640295ae156856b
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-08-04 09:45:23 +0000

    feat(main): used new parser

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 mod cmdline;
 mod error;
 mod lexer;
+mod parser;
 
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    process::exit,
 };
 
 use clap::Parser;
 use error::Error;
 use lexer::{LexerState, token::Token, tokenize};
+use parser::node::Statement;
 
 pub fn wrapper() -> Result<(), Error> {
     let cli = cmdline::Cli::parse();
 
     let oxygen_file = File::open(cli.oxygen_file)?;
-    dbg!(&oxygen_file);
 
     let mut lexer_state = LexerState::new();
     let mut tokens: Vec<Token> = Vec::new();
@@ -27,8 +29,18 @@ pub fn wrapper() -> Result<(), Error> {
 
     if cli.display_tokens {
         for token in &tokens {
-            println!("{token}");
+            println!("{token:#?}");
         }
+        return Ok(());
+    }
+
+    let mut prog: Vec<Statement> = Vec::new();
+    let mut parser = parser::Parser::new(tokens, &mut prog);
+    parser.parse()?;
+
+    if cli.display_ast {
+        println!("{prog:#?}");
+        return Ok(());
     }
 
     Ok(())
@@ -37,6 +49,6 @@ pub fn wrapper() -> Result<(), Error> {
 pub fn main() {
     if let Err(e) = wrapper() {
         eprintln!("\x1b[31;1merror:\x1b[0m {e}");
-        std::process::exit(1);
+        exit(1);
     }
 }
